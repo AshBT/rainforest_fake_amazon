04ec27296b8dff7d3bf40da11ba696c966832ffd
AshBt <ashley.beattie@gmail.com>
2013-03-25 13:37:16 -0400

    Add price_in_dollars method

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -7,6 +7,7 @@ class ProductTest < ActiveSupport::TestCase
 
   setup do
   	@p = FactoryGirl.create(:second_product)
+  	@p2 = FactoryGirl.create(:second_product)
   	assert @p.valid?
   end
 
@@ -17,10 +18,18 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product must have a name" do
-  	@p2 = FactoryGirl.create(:second_product)
   	@p2.name = ""
   	refute @p2.valid?
   end
 
+  test "price_in_cents must be an integer" do
+  	@p.price_in_cents = 0.1
+  	assert !@p.valid?
+  end
+
+  test "must have a price_in_cents" do
+    @p.price_in_cents = nil
+    refute @p.valid?
+  end
 
 end
