8704ada14f9b98e67f0d7d898bd979d68d5341c9
Bitmaker Labs <bitmakerlabs@Bitmakers-iMac-2.local>
2013-03-25 08:20:48 -0400

    first commit -- rainforest app -- just created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rainforest::Application.config.secret_token = '38d6528cacde754e6f872608c54d2a7dfa32799a14ffa469d993e7215e5c4c317504d27e24077836f0b6f9fce81dff6fccb9b6afdde2242aa34e1bcfd92b6b5e'
