08455c684c2cbd842cecea99fbcb416d1687c853
AshBt <ashley.beattie@gmail.com>
2013-03-26 17:07:46 -0400

    add reviews form functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,76 +1,30 @@
 class ReviewsController < ApplicationController
-  # GET /reviews
-  # GET /reviews.json
+
+  before_filter :load_product
+
   def index
     @reviews = Review.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @reviews }
-    end
   end
 
-  # GET /reviews/1
-  # GET /reviews/1.json
   def show
     @review = Review.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @review }
-    end
   end
 
-  # GET /reviews/new
-  # GET /reviews/new.json
-  def new
-    @review = Review.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @review }
-    end
-  end
-
-  # GET /reviews/1/edit
-  def edit
-    @review = Review.find(params[:id])
-  end
-
-  # POST /reviews
-  # POST /reviews.json
   def create
-    @review = Review.new(params[:review])
+    # @review = Review.new(:comment => params[:review][:comment], :product_id => @product.id, :user_id => current_user.id)
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render json: @review, status: :created, location: @review }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    @review = @product.reviews.build(
+      :comment => params[:review][:comment],
+      :user_id => current_user.id
+      )
+
+    if @review.save
+      redirect_to @product, notice: "Review was successfully created."
+    else
+        render action: "../products/show"
     end
   end
 
-  # PUT /reviews/1
-  # PUT /reviews/1.json
-  def update
-    @review = Review.find(params[:id])
-
-    respond_to do |format|
-      if @review.update_attributes(params[:review])
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1
-  # DELETE /reviews/1.json
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
@@ -80,4 +34,9 @@ class ReviewsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def load_product
+    @product = Product.find(params[:product_id])
+  end
+
 end
